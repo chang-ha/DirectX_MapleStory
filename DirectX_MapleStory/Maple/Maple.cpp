5dfe0f2724012e4081151ad831ac2ef7b3f5eaf2
chang-ha <kym637@naver.com>
2023-08-23 02:44:55 +0900

    우창하 : Poco::NotFoundException에러 해결

--- DirectX_MapleStory/Maple/Maple.cpp
+++ DirectX_MapleStory/Maple/Maple.cpp
@@ -16,3 +16,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	GameEngineCore::EngineStart<MapleStoryCore>(hInstance);
 }
 
+// 예외 발생(0x00007FFCC08FCF19, Maple.exe): Microsoft C++ 예외: Poco::NotFoundException, 메모리 위치 0x000000835D0F2370. << 이런 에러가 SwapChain에서 계속 발생
+// 모든 코드를 헤집어도 해결되지 않음
+// 결론 : 실행파일을 만드는 프로젝트의 이름이 너무 길면 나오는 에러임;;;
+// 프로젝트명을 MapleStory -> Maple로 줄였더니 해결;;
