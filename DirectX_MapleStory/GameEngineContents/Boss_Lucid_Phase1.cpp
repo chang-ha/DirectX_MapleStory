3934819dfd85ea9b76904bafd4a47cd6b1a37340
chang-ha <kym637@naver.com>
2023-09-26 01:38:44 +0900

    우창하 : 루시드 기본 모션들 추가

--- DirectX_MapleStory/GameEngineContents/Boss_Lucid_Phase1.cpp
+++ DirectX_MapleStory/GameEngineContents/Boss_Lucid_Phase1.cpp
@@ -24,8 +24,8 @@ void Boss_Lucid_Phase1::LevelEnd(GameEngineLevel* _NextLevel)
 void Boss_Lucid_Phase1::Start()
 {
 	FlowerRenderer = CreateComponent<GameEngineSpriteRenderer>(RenderOrder::Monster);
-	BaseBossActor::Start();
 	FlowerRenderer->AutoSpriteSizeOn();
+	BaseBossActor::Start();
 
 	if (nullptr == GameEngineSprite::Find("Lucid_Phase1_Death"))
 	{
@@ -39,28 +39,20 @@ void Boss_Lucid_Phase1::Start()
 			GameEngineDirectory& Childs = Directorys[i];
 			GameEngineSprite::CreateFolder("Lucid_Phase1_" + Childs.GetFileName(), Childs.GetStringPath());
 		}
+
+		Dir.MoveParentToExistsChild("Phase1");
+		Dir.MoveChild("Attack");
+		GameEngineSprite::CreateFolder("Lucid_" + Dir.GetFileName(), Dir.GetStringPath());
 	}
 
-
 	BossRenderer->CreateAnimation("Idle", "Lucid_Phase1_Idle");
+	BossRenderer->CreateAnimation("Skill1", "Lucid_Phase1_Skill1", 0.12f);
+	BossRenderer->CreateAnimation("Skill2", "Lucid_Phase1_Skill2", 0.09f);
+	BossRenderer->CreateAnimation("Skill3", "Lucid_Phase1_Skill3", 0.09f);
+	BossRenderer->CreateAnimation("Skill4", "Lucid_Phase1_Skill4", 0.09f);
 	BossRenderer->CreateAnimation("Death", "Lucid_Phase1_Death", 0.11f);
 	BossRenderer->ChangeAnimation("Idle");
-
-	BossRenderer->SetStartEvent("Idle", [&](GameEngineRenderer* _Renderer)
-		{
-			BossRenderer->SetPivotType(PivotType::Center);
-			BossRenderer->Transform.SetLocalPosition({37, 321});
-			FlowerRenderer->On();
-		}
-	);
-
-	BossRenderer->SetStartEvent("Death", [&](GameEngineRenderer* _Renderer)
-		{
-			BossRenderer->SetPivotType(PivotType::Bottom);
-			BossRenderer->Transform.SetLocalPosition({});
-			FlowerRenderer->Off();
-		}
-	);
+	BossRenderer->Transform.SetLocalPosition({ 37, 321 });
 
 	FlowerRenderer->CreateAnimation("Flower", "Lucid_Phase1_Flower", 0.15f);
 	FlowerRenderer->SetPivotType(PivotType::Bottom);
@@ -71,15 +63,219 @@ void Boss_Lucid_Phase1::Start()
 void Boss_Lucid_Phase1::Update(float _Delta)
 {
 	BaseBossActor::Update(_Delta);
+	StateUpdate(_Delta);
 
 	// TestCode
+	if (true == GameEngineInput::IsDown('5'))
+	{
+		ChangeState(LucidState::Skill1);
+	}
+
+	if (true == GameEngineInput::IsDown('6'))
+	{
+		ChangeState(LucidState::Skill2);
+	}
+
+	if (true == GameEngineInput::IsDown('7'))
+	{
+		ChangeState(LucidState::Skill3);
+	}
+
+	if (true == GameEngineInput::IsDown('8'))
+	{
+		ChangeState(LucidState::Skill4);
+	}
+
 	if (true == GameEngineInput::IsDown('9'))
 	{
-		BossRenderer->ChangeAnimation("Idle");
+		ChangeState(LucidState::Idle);
 	}
 
 	if (true == GameEngineInput::IsDown('0'))
 	{
-		BossRenderer->ChangeAnimation("Death");
+		ChangeState(LucidState::Death);
 	}
+}
+
+void Boss_Lucid_Phase1::ChangeState(LucidState _State)
+{
+	if (_State != State)
+	{
+		// State End
+		switch (State)
+		{
+		case LucidState::Idle:
+			IdleEnd();
+			break;
+		case LucidState::Death:
+			DeathEnd();
+			break;
+		case LucidState::Skill1:
+			Skill1End();
+			break;
+		case LucidState::Skill2:
+			Skill2End();
+			break;
+		case LucidState::Skill3:
+			Skill3End();
+			break;
+		case LucidState::Skill4:
+			Skill4End();
+			break;
+		default:
+			MsgBoxAssert("존재하지 않는 상태값으로 변경하려고 했습니다.");
+			break;
+		}
+
+		// State Start
+		switch (_State)
+		{
+		case LucidState::Idle:
+			IdleStart();
+			break;
+		case LucidState::Death:
+			DeathStart();
+			break;
+		case LucidState::Skill1:
+			Skill1Start();
+			break;
+		case LucidState::Skill2:
+			Skill2Start();
+			break;
+		case LucidState::Skill3:
+			Skill3Start();
+			break;
+		case LucidState::Skill4:
+			Skill4Start();
+			break;
+		default:
+			break;
+		}
+	}
+
+	State = _State;
+}
+
+void Boss_Lucid_Phase1::StateUpdate(float _Delta)
+{
+	switch (State)
+	{
+	case LucidState::Idle:
+		return IdleUpdate(_Delta);
+	case LucidState::Death:
+		return DeathUpdate(_Delta);
+	case LucidState::Skill1:
+		return Skill1Update(_Delta);
+	case LucidState::Skill2:
+		return Skill2Update(_Delta);
+	case LucidState::Skill3:
+		return Skill3Update(_Delta);
+	case LucidState::Skill4:
+		return Skill4Update(_Delta);
+	default:
+		MsgBoxAssert("존재하지 않는 상태값으로 Update를 돌릴 수 없습니다.");
+		break;
+	}
+}
+
+
+void Boss_Lucid_Phase1::IdleStart()
+{
+	BossRenderer->SetPivotType(PivotType::Center);
+	BossRenderer->Transform.SetLocalPosition({ 37, 321 });
+	BossRenderer->ChangeAnimation("Idle");
+	FlowerRenderer->On();
+}
+
+void Boss_Lucid_Phase1::DeathStart()
+{
+	BossRenderer->SetPivotType(PivotType::Bottom);
+	BossRenderer->Transform.SetLocalPosition({});
+	BossRenderer->ChangeAnimation("Death");
+	FlowerRenderer->Off();
+}
+
+void Boss_Lucid_Phase1::Skill1Start()
+{
+	BossRenderer->SetPivotValue({ 0.433f, 0.677f });
+	BossRenderer->ChangeAnimation("Skill1");
+}
+
+void Boss_Lucid_Phase1::Skill2Start()
+{
+	BossRenderer->SetPivotValue({ 0.274f, 0.706f });
+	BossRenderer->ChangeAnimation("Skill2");
+}
+
+void Boss_Lucid_Phase1::Skill3Start()
+{
+	BossRenderer->SetPivotValue({ 0.215f, 0.648f });
+	BossRenderer->ChangeAnimation("Skill3");
+}
+
+void Boss_Lucid_Phase1::Skill4Start()
+{
+	BossRenderer->SetPivotValue({ 0.452f, 0.444f });
+	BossRenderer->ChangeAnimation("Skill4");
+}
+
+void Boss_Lucid_Phase1::IdleUpdate(float _Delta)
+{
+
+}
+
+void Boss_Lucid_Phase1::DeathUpdate(float _Delta)
+{
+
+}
+
+void Boss_Lucid_Phase1::Skill1Update(float _Delta)
+{
+
+}
+
+void Boss_Lucid_Phase1::Skill2Update(float _Delta)
+{
+
+}
+
+void Boss_Lucid_Phase1::Skill3Update(float _Delta)
+{
+
+}
+
+void Boss_Lucid_Phase1::Skill4Update(float _Delta)
+{
+
+}
+
+void Boss_Lucid_Phase1::IdleEnd()
+{
+
+}
+
+void Boss_Lucid_Phase1::DeathEnd()
+{
+	BossRenderer->Transform.SetLocalPosition({ 37, 321 });
+	FlowerRenderer->On();
+}
+
+void Boss_Lucid_Phase1::Skill1End()
+{
+
+}
+
+void Boss_Lucid_Phase1::Skill2End()
+{
+
+}
+
+void Boss_Lucid_Phase1::Skill3End()
+{
+
+}
+
+void Boss_Lucid_Phase1::Skill4End()
+{
+
 }
--- DirectX_MapleStory/GameEngineContents/Boss_Lucid_Phase1.h
+++ DirectX_MapleStory/GameEngineContents/Boss_Lucid_Phase1.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "BaseBossActor.h"
 
+enum class LucidState
+{
+	Idle,
+	Death,
+	Skill1, // Shoot
+	Skill2, // Summon Dragon
+	Skill3, // Teleport
+	Skill4, // Summon Butterfly
+};
+
 class Boss_Lucid_Phase1 : public BaseBossActor
 {
 public:
@@ -14,6 +24,9 @@ public:
 	Boss_Lucid_Phase1& operator=(const Boss_Lucid_Phase1& _Other) = delete;
 	Boss_Lucid_Phase1& operator=(Boss_Lucid_Phase1&& _Other) noexcept = delete;
 
+	void ChangeState(LucidState _State);
+	void StateUpdate(float _Delta);
+
 protected:
 	void LevelStart(GameEngineLevel* _PrevLevel) override;
 	void LevelEnd(GameEngineLevel* _NextLevel) override;
@@ -22,5 +35,33 @@ protected:
 	void Update(float _Delta) override;
 private:
 	std::shared_ptr<GameEngineSpriteRenderer> FlowerRenderer = nullptr;
+	LucidState State = LucidState::Idle;
+
+	///// State
+	// Start
+	void IdleStart();
+	void DeathStart();
+	void Skill1Start(); 
+	void Skill2Start(); 
+	void Skill3Start();
+	void Skill4Start();
+	//
+	// Update
+	void IdleUpdate(float _Delta);
+	void DeathUpdate(float _Delta);
+	void Skill1Update(float _Delta);
+	void Skill2Update(float _Delta);
+	void Skill3Update(float _Delta);
+	void Skill4Update(float _Delta);
+	// 
+	// End
+	void IdleEnd();
+	void DeathEnd();
+	void Skill1End();
+	void Skill2End();
+	void Skill3End();
+	void Skill4End();
+	// 
+	/////
 };
 
