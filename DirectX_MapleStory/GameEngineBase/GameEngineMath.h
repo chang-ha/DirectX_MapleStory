0b13b257d4f22870a22fb2fc69e3db3c85f5f78a
chang-ha <kym637@naver.com>
2023-11-05 23:43:32 +0900

    우창하 : GameEngineMath 반올림, 올림, 내림 함수 추가

--- DirectX_MapleStory/GameEngineBase/GameEngineMath.h
+++ DirectX_MapleStory/GameEngineBase/GameEngineMath.h
@@ -504,7 +504,44 @@ public:
 		return Result;
 	}
 
+	inline float4 RoundUpReturn() const
+	{
+		float4 Result = *this;
+		Result.X = round(Result.X);
+		Result.Y = round(Result.Y);
+		Result.Z = round(Result.Z);
+		return Result;
+	}
 
+	inline float4 RoundDownReturn() const
+	{
+		float4 Result = *this;
+		Result.X = floor(Result.X);
+		Result.Y = floor(Result.Y);
+		Result.Z = floor(Result.Z);
+		return Result;
+	}
+
+	inline void Ceil()
+	{
+		X = ceil(X);
+		Y = ceil(Y);
+		Z = ceil(Z);
+	}
+
+	inline void Round()
+	{
+		X = round(X);
+		Y = round(Y);
+		Z = round(Z);
+	}
+
+	inline void Floor()
+	{
+		X = floor(X);
+		Y = floor(Y);
+		Z = floor(Z);
+	}
 
 	float4 operator*(const class float4x4& _Other) const;
 	float4& operator*=(const class float4x4& _Other);
