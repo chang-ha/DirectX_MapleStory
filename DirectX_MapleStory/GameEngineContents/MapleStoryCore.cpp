bbd18e1c01a937fde35067f1fddb661a3d5944b7
chang-ha <kym637@naver.com>
2023-12-07 14:11:08 +0900

    우창하 : 업데이트

--- DirectX_MapleStory/GameEngineContents/MapleStoryCore.cpp
+++ DirectX_MapleStory/GameEngineContents/MapleStoryCore.cpp
@@ -80,7 +80,7 @@ void MapleStoryCore::Start()
 	GameEngineCore::CreateLevel<Lucid_Phase2>("14.Lucid_Phase2");
 	GameEngineCore::CreateLevel<Boss_ClearRoom>("15.Boss_ClearRoom");
 
-	GameEngineCore::ChangeLevel("04.Lachlen");
+	GameEngineCore::ChangeLevel("01.TitleLevel");
 }
 
 
